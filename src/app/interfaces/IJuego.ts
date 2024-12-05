9bae6d7d6641c3dda409aff046595b606b1c916e
Jeremias Storm <jerestorm@gmail.com>
2024-12-05 12:43:45 -0300

    Se arregla bug node_modules

--- src/app/interfaces/IJuego.ts
+++ src/app/interfaces/IJuego.ts
@@ -1,9 +1,11 @@
+import { Jugador } from "../Jugador";
+
 export interface IJuego {
-    iniciar(jugador : Jugador); 
-    comoJugar();
-    insertarApuesta(apuesta : number);
-    jugarRonda();
-    finalizar() : Jugador;
-    tipoDeApuesta();
+    iniciar(jugador: Jugador): void;
+    comoJugar(): void;
+    insertarApuesta(apuesta: number): void;
+    jugarRonda(): void;
+    finalizar(): Jugador;
+    tipoDeApuesta(): void;
 
 }
